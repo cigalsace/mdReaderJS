c0ca8499ff276f810eed4e922a55ea778786a763
Guillaume RYCKELYNCK <guillaume@ryckelynck.info>
2015-07-02 22:23:14 +0200

    Version 0.5 - Mise à jour du 150702

--- js/mdParser.js
+++ js/mdParser.js
@@ -84,9 +84,8 @@ function readXML(xml) {
         Data_LI_Statement: $(xml).find(xpaths.Data_LI_Statement).text(),
         Data_DQ_Conformities: getConformities($(xml))
     }
-    data.md = md;
-    data.lb = lb[lang];
-    return data;
+    //data.md = md;
+    return md;
 }
 function getFileIdentifier(xml) {
     return $(xml).find(xpaths.MD_FileIdentifier).text();
